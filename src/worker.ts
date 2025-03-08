3e1ab784b580991a6cebcf2e38f492dd72afb0f8
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2025-03-08 22:26:21 +0100

    Scaffold worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,2 +1,17 @@
-console.error("[ERROR] Not implemented");
-process.exit(1);
+import { Worker } from "@temporalio/worker";
+import { QUEUE_NAME } from "./shared/consts.js";
+
+async function run() {
+  const worker = await Worker.create({
+    taskQueue: QUEUE_NAME,
+  });
+
+  await worker.run();
+}
+
+try {
+  await run();
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
